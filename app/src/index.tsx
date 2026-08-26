6b283b308e46309061ed79782a243fd5807b376e
Tuong Tan <helijackybu@gmail.com>
2026-08-26 15:42:08 +1000

    Add analytics

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -4,11 +4,13 @@ import './styles/App.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store/Store';
+import { Analytics } from "@vercel/analytics/react"
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Analytics></Analytics>
       <App />
     </Provider>
   </React.StrictMode>
